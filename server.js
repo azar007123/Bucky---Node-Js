71d020fb218761e961754c266056092423644af0
jayweb <jayrdee@gmail.com>
2015-10-30 16:30:31 +0100

    tutorial at 3:41 of connect, CANNOT GET/ part, server is running

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 /**
  * Created by jayweb on 10/30/2015.
  */
+var connect = require('connect');
+var http  = require('http');
+
+var app = connect();
+
+http.createServer(app).listen(8888);
+console.log('server is running ....');
